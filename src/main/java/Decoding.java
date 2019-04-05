2b28635a1f4a9fc58aa16e48cc41e760211b18ae
iamkosgei <kosgeik2930@gmail.com>
2019-04-05 17:19:49 +0300

    added test to check whether it saves the decoded text

--- src/main/java/Decoding.java
+++ src/main/java/Decoding.java
@@ -1,3 +1,9 @@
 public class Decoding {
-    
+    int mKey;
+    String mInputText;
+
+    public Decoding(int key, String inputText) {
+        mKey = key;
+        mInputText = inputText;
+    }
 }
--- src/test/java/DecodingTest.java
+++ src/test/java/DecodingTest.java
@@ -3,5 +3,11 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DecodingTest {
-    Decoding decoding = new Encoding("MGNXKP MQUIGK", 2);
+    Decoding decoding = new Decoding( 2, "MGNXKP MQUIGK");
+
+    @Test
+    public void checkIfSavesInputText()
+    {
+        assertEquals("MGNXKP MQUIGK", decoding.getInputText() );
+    }
 }
