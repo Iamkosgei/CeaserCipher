b4684f513586e619513e88c989713ed8d7bf552d
iamkosgei <kosgeik2930@gmail.com>
2019-04-05 14:25:01 +0300

    added test to check whether input text is saved and implemented logic

--- src/main/java/Encoding.java
+++ src/main/java/Encoding.java
@@ -2,9 +2,12 @@ public class Encoding {
     private int mKey;
     private String mInputText;
 
-    public Encoding(int key, String inputText) {
+    public Encoding(String inputText,int key) {
         mKey = key;
         mInputText = inputText;
     }
 
+    public String getInputText() {
+        return mInputText;
+    }
 }
--- src/test/java/EncodingTest.java
+++ src/test/java/EncodingTest.java
@@ -3,11 +3,17 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class EncodingTest {
+    Encoding testEncoding = new Encoding("KELVIN", 2);
+    @Test
+    public void checkIfItCreatesInstance()
+    {
+        assertEquals(true, testEncoding instanceof Encoding);
+    }
 
     @Test
-    public void createInstance()
+    public void checkIfSavesInputText()
     {
-
+        assertEquals("KELVIN", testEncoding.getInputText() );
     }
 
 }
